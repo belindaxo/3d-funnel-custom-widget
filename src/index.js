d8da8b3ca7f442d05853699e68b0512007a70fae
Belinda <bmdebruyn@gmail.com>
2024-08-22 12:07:35 -0600

    debug data label formatter

--- src/index.js
+++ src/index.js
@@ -190,7 +190,7 @@ var parseMetadata = metadata => {
                             formatter: function () {
                                 const category = categoryData[0].data[series[0].data.indexOf(this.y)];
                                 const value = scaleFormat(this.y);
-                                return `${category.name} - ${value}`;
+                                return `${category.description} - ${value}`;
                             },
                             y: 10
                         },
